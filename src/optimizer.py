5512a39a4ac9e8963933b790c5bec80bb5d0b26e
wahur666 <imre.secodi@gmail.com>
2019-05-15 01:07:07 +0200

    rekurzió még nem jól megy

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from typing import Tuple
 
 from implementation import *
 
+FLIPPABLE_OPERANDS = ["+", "*", "and", "or"]
 
 class OptimizableSymbol:
 
@@ -45,9 +47,9 @@ class Optimizer:
     def optimalize_const_merge(self, commands: List[Instruction]):
         for command in commands:
             if isinstance(command, AssignInstruction):
-                opt_struct = self.opt_expresstion(command.right)
+                opt_struct = self.opt_expresstion(command, command.right)
                 #print(opt_struct)
-                if opt_struct.optimizable:
+                if opt_struct and opt_struct.optimizable:
                     command.right = self.simpler_node(opt_struct)
                     self.opt_table[command.left] = OptimizableSymbol(command.left, opt_struct.value, True)
                 else:
@@ -57,25 +59,25 @@ class Optimizer:
                 self.opt_table[command.id] = OptimizableSymbol(command.id)
 
             elif isinstance(command, WriteInstruction):
-                opt_struct = self.opt_expresstion(command.exp)
+                opt_struct = self.opt_expresstion(command, command.exp)
                 if opt_struct.optimizable:
                     command.exp = self.simpler_node(opt_struct)
 
             elif isinstance(command, IfInstruction):
-                opt_struct = self.opt_expresstion(command.condition)
+                opt_struct = self.opt_expresstion(command, command.condition)
                 if opt_struct.optimizable:
                     command.condition = self.simpler_node(opt_struct)
                 self.optimalize_const_merge(command.true_branch)
                 self.optimalize_const_merge(command.false_branch)
 
             elif isinstance(command, WhileInstruction):
-                opt_struct = self.opt_expresstion(command.condition)
+                opt_struct = self.opt_expresstion(command, command.condition)
                 if opt_struct.optimizable:
                     command.condition = self.simpler_node(opt_struct)
                 self.optimalize_const_merge(command.body)
 
             elif isinstance(command, RepeatInstruction):
-                opt_struct = self.opt_expresstion(command.count)
+                opt_struct = self.opt_expresstion(command, command.count)
                 if opt_struct.optimizable:
                     command.condition = self.simpler_node(opt_struct)
                 self.optimalize_const_merge(command.body)
@@ -83,7 +85,7 @@ class Optimizer:
         #print_commands(0, commands)
 
 
-    def opt_expresstion(self, exp: Expression) -> OptStruct:
+    def opt_expresstion(self, root: Instruction, exp: Expression) -> OptStruct:
         if isinstance(exp, NumberExpression):
             return OptStruct(True, exp.get_value(), NATURAL)
 
@@ -97,24 +99,24 @@ class Optimizer:
                 return OptStruct(False, -1, -1)
 
         elif isinstance(exp, NotExpression):
-            opt_struct = self.opt_expresstion(exp.operand)
+            opt_struct = self.opt_expresstion(root, exp.operand)
             if opt_struct.optimizable:
                 return OptStruct(True, not opt_struct.value, BOOLEAN)
             else:
                 return OptStruct(False, -1, -1)
 
         elif isinstance(exp, BinopExpression):
-            opt_struct1 = self.opt_expresstion(exp.left)
-            if opt_struct1.optimizable:
-                exp.left = self.simpler_node(opt_struct1)
+            main_expression_left = self.opt_expresstion(root, exp.left)
+            if main_expression_left.optimizable:
+                exp.left = self.simpler_node(main_expression_left)
 
-            opt_struct2 = self.opt_expresstion(exp.right)
-            if opt_struct2.optimizable:
-                exp.right = self.simpler_node(opt_struct2)
+            main_expression_right = self.opt_expresstion(root, exp.right)
+            if main_expression_right.optimizable:
+                exp.right = self.simpler_node(main_expression_right)
 
-            if opt_struct1.optimizable and opt_struct2.optimizable:
-                left_value = opt_struct1.value
-                right_value = opt_struct2.value
+            if main_expression_left.optimizable and main_expression_right.optimizable:
+                left_value = main_expression_left.value
+                right_value = main_expression_right.value
                 if exp.op == "+":
                     value =  left_value + right_value
                     type = NATURAL
@@ -152,43 +154,126 @@ class Optimizer:
                     value = left_value == right_value
                     type = BOOLEAN
                 return OptStruct(True, value, type)
-            elif opt_struct1.optimizable and not opt_struct2.optimizable:
-                if isinstance(exp.right, BinopExpression):
-                    right: BinopExpression = exp.right
-                    if right.op == exp.op and exp.op in ["+", "*", "=", "and", "or"]:
-                        t_opt_struct_left = self.opt_expresstion(right.left)
-                        t_opt_struct_right = self.opt_expresstion(right.right)
-                        if t_opt_struct_left.optimizable:
-                            new_opt_exp = BinopExpression(exp.line, exp.op, exp.left, right.left)
-                            new_exp = BinopExpression(exp.line, exp.op, new_opt_exp, right.right)
-                            exp = new_exp
-                            return self.opt_expresstion(exp)
-                        elif t_opt_struct_right.optimizable:
-                            new_opt_exp = BinopExpression(exp.line, exp.op, exp.left, right.right)
-                            new_exp = BinopExpression(exp.line, exp.op, new_opt_exp, right.left)
-                            exp = new_exp
-                            return self.opt_expresstion(exp)
+            elif not main_expression_left.optimizable and main_expression_right.optimizable:
+                ''' a jobb oldal optimalizalhato
+                    megézzük hogy a bal oldal miért nem optimalizálható
+                    nézzük meg összetett kifelyezés van-e a bal oldalon '''
+
+                if isinstance(exp.left, BinopExpression):
+                    not_optimalizable_expression: BinopExpression = exp.left
+                    ''' nézzük meg hogy megegyeznek-e az operátorok, a nem kiértékelhető
+                        és a rákövetkező műveletnél, és az asszociativ-e '''
+
+                    if not_optimalizable_expression.op == exp.op and exp.op in FLIPPABLE_OPERANDS:
+                        'bal oldalon ki kell derinteni miert nem optimalizalhato'
+                        not_optimalizable_left_expression = self.opt_expresstion(root, not_optimalizable_expression.left)
+                        not_optimalizable_right_expression = self.opt_expresstion(root, not_optimalizable_expression.right)
+
+                        if not_optimalizable_left_expression.optimizable:
+                            'ha bal oldal optimalizalhato akkor a jobb a gond'
+                            new_optimlaizable_expression = BinopExpression(exp.line, exp.op, exp.right, not_optimalizable_expression.left)
+                            optimalized_expression = self.simpler_node(self.opt_expresstion(root, new_optimlaizable_expression))
+                            new_expression = BinopExpression(exp.line, exp.op, not_optimalizable_expression.right, optimalized_expression)
+                            # exp = new_expression
+                            self.replace_expression(root, exp, new_expression)
+                            return self.opt_expresstion(root, exp)
+
+                        elif not_optimalizable_right_expression.optimizable:
+                            'ha jobb oldal optimalizalhato akkor a bal a gond'
+                            new_optimlaizable_expression = BinopExpression(exp.line, exp.op, exp.right, not_optimalizable_expression.right)
+                            optimalized_expression = self.simpler_node(self.opt_expresstion(root, new_optimlaizable_expression))
+                            new_expression = BinopExpression(exp.line, exp.op, not_optimalizable_expression.left, optimalized_expression)
+                            # exp = new_expression
+                            self.replace_expression(root, exp, new_expression)
+                            return self.opt_expresstion(root, exp)
                         else:
                             return OptStruct(False, -1, -1)
                 else:
                     return OptStruct(False, -1, -1)
 
-            elif not opt_struct1.optimizable and opt_struct2.optimizable:
-                if isinstance(exp.left, BinopExpression):
-                    return OptStruct(False, -1, -1)
-                else:
-                    return OptStruct(False, -1, -1)
+            elif main_expression_left.optimizable and not main_expression_right.optimizable:
+                'ez az eset nem letezik, mivel minden perator balra köt'
+                return OptStruct(False, -1, -1)
             else:
+                'ez csak akkor lehet ha egyszerre tobb nem kotott valtozo van'
+                if isinstance(exp.left, BinopExpression):
+                    not_optimalizable_expression: BinopExpression = exp.left
+                    'megkeressuk van e legalabb 1 dolog amit ki lehet hozni'
+
+                    if not_optimalizable_expression.op == exp.op and exp.op in FLIPPABLE_OPERANDS:
+                        not_optimalizable_left_expression = self.opt_expresstion(root, not_optimalizable_expression.left)
+                        not_optimalizable_right_expression = self.opt_expresstion(root, not_optimalizable_expression.right)
+
+                        if not_optimalizable_left_expression.optimizable:
+                            'bal oldalon van egy optimalizalhato'
+                            'csoportositjuk a ket nem optimalizalhatot'
+                            new_not_optimalizable_expression = BinopExpression(exp.line, exp.op, not_optimalizable_expression.right, exp.right)
+                            new_expression = BinopExpression(exp.line, exp.op, new_not_optimalizable_expression, not_optimalizable_expression.left)
+                            # exp = new_expression
+                            self.replace_expression(root, exp, new_expression)
+                            return self.opt_expresstion(root, exp)
+                        elif not_optimalizable_right_expression.optimizable:
+                            'jobb oldalon van egy optimalizalhato'
+                            'csoportositjuk a ket nem optimalizalhatot'
+                            new_not_optimalizable_expression = BinopExpression(exp.line, exp.op, not_optimalizable_expression.left, exp.right)
+                            new_expression = BinopExpression(exp.line, exp.op, new_not_optimalizable_expression,
+                                                             not_optimalizable_expression.right)
+                            #exp = new_expression
+                            self.replace_expression(root, exp, new_expression)
+                            return self.opt_expresstion(root, exp)
+                        else:
+                            return OptStruct(False, -1, -1)
+
                 return OptStruct(False, -1, -1)
 
         elif isinstance(exp, TernaryExpression):
-            opt_struct = self.opt_expresstion(exp.condition)
+            opt_struct = self.opt_expresstion(root, exp.condition)
 
             if opt_struct.optimizable:
                 exp.condition = self.simpler_node(opt_struct)
                 if opt_struct.value:
-                    return self.opt_expresstion(exp.true_expression)
+                    return self.opt_expresstion(root, exp.true_expression)
                 else:
-                    return self.opt_expresstion(exp.false_expression)
+                    return self.opt_expresstion(root, exp.false_expression)
             else:
                 return OptStruct(False, -1, -1)
+
+    def replace_expression(self, instrcution: Instruction, old_expression: Expression, new_expression: Expression):
+        if isinstance(instrcution, AssignInstruction):
+            if instrcution.right == old_expression:
+                instrcution.right = new_expression
+            else:
+                self.search_and_replace_expression(instrcution.right, old_expression, new_expression)
+        elif isinstance(instrcution, WriteInstruction):
+            if instrcution.exp == old_expression:
+                instrcution.exp = new_expression
+            else:
+                self.search_and_replace_expression(instrcution.exp, old_expression, new_expression)
+        elif isinstance(instrcution, IfInstruction):
+            if instrcution.condition == old_expression:
+                instrcution.condition = new_expression
+            else:
+                self.search_and_replace_expression(instrcution.condition, old_expression, new_expression)
+        elif isinstance(instrcution, WhileInstruction):
+            if instrcution.condition == old_expression:
+                instrcution.condition = new_expression
+            else:
+                self.search_and_replace_expression(instrcution.condition, old_expression, new_expression)
+        elif isinstance(instrcution, RepeatInstruction):
+            if instrcution.count == old_expression:
+                instrcution.count = new_expression
+            else:
+                self.search_and_replace_expression(instrcution.count, old_expression, new_expression)
+
+        return instrcution
+
+    def search_and_replace_expression(self, root_expression: Expression, old_expression: Expression, new_expression: Expression):
+        if isinstance(root_expression, BinopExpression):
+            if root_expression.right == old_expression:
+                root_expression.right = new_expression
+            elif root_expression.left == old_expression:
+                root_expression.left = new_expression
+            else:
+                self.search_and_replace_expression(root_expression.right, old_expression, new_expression)
+                self.search_and_replace_expression(root_expression.left, old_expression, new_expression)
+
